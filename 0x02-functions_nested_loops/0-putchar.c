7acfc2ff90ef7244136539699cf2697a4387cf32
mahmoud <mahmoudabdelaal672@gmail.com>
2023-08-16 09:26:21 -0700

    var

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unstd.h>
+/**
+ * main - This is the _putchar
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	  char str[] = "_putchar\n";
+	    write(1, str, sizeof(str) - 1);
+	      return (0);
+}
